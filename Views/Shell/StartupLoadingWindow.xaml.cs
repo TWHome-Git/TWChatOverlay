749b55e9fbee7b64288bb7d275b8cde765af8406
TWHome-Git <talesweaverhome@gmail.com>
2026-05-13 14:17:16 +0900

    Update 3.1.0

--- Views/Shell/StartupLoadingWindow.xaml.cs
+++ Views/Shell/StartupLoadingWindow.xaml.cs
@@ -1,9 +1,12 @@
+﻿using System;
 using System.Windows;
 
 namespace TWChatOverlay.Views
 {
     public partial class StartupLoadingWindow : Window
     {
+        public event EventHandler? CancelRequested;
+
         public StartupLoadingWindow()
         {
             InitializeComponent();
@@ -24,6 +27,18 @@ namespace TWChatOverlay.Views
             DateText.Text = string.IsNullOrWhiteSpace(dateText)
                 ? string.Empty
                 : $"현재 처리: {dateText}";
+        }
+
+        public void SetCancelEnabled(bool enabled)
+        {
+            CancelButton.IsEnabled = enabled;
+            CancelButton.Content = enabled ? "취소" : "취소됨";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetCancelEnabled(false);
+            CancelRequested?.Invoke(this, EventArgs.Empty);
         }
     }
 }
